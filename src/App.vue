70517e38876e057ca91645a5cbe13627d65972cc
Tom <1755619015@qq.com>
2023-05-25 14:14:39 +0800

    feat: add axios request twice

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,23 @@
   </div>
 </template>
 
-<script></script>
+<script setup lang="ts">
+import request from '@/utils/request'
+import { onMounted } from 'vue'
 
-<style>
-</style>
+// 组件挂载完毕，测试发送一个请求
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((response) => {
+    console.log(response)
+  })
+})
+</script>
+
+<style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // mock插件提供的方法
-import { viteMockServe } from 'vite-plugin-mock';
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {

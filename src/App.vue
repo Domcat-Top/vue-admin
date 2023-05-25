5025fb8b27e0e128e7ffbe30e97cda45a2246bce
Tom <1755619015@qq.com>
2023-05-25 14:37:49 +0800

    feat: finish userInterface

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <template>
-  <div>
-    <h1>根组件</h1>
-    <SvgIcon name="loading"></SvgIcon>
-  </div>
+  <div><h1>根组件</h1></div>
 </template>
 
 <script setup lang="ts">
-import request from '@/utils/request'
-import { onMounted } from 'vue'
 
-// 组件挂载完毕，测试发送一个请求
-onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((response) => {
-    console.log(response)
-  })
-})
 </script>
 
-<style></style>
+<style>
+</style>
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,57 +1,57 @@
-
 // 进行 axios 的二次封装：使用它的请求和响应拦截器
-import axios from 'axios';
-import { ElMessage } from 'element-plus';
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 // 使用axios对象的create方法，去创建axios实例
 // 基础路径、请求超时时间等
 let request = axios.create({
-    // 基础路径----自动的帮我们加上了这个基础url
-    baseURL: import.meta.env.VITE_APP_BASE_API, // 基础路径上会自动携带/api
-    timeout: 5000, // 超市的时间设置
-});
+  // 基础路径----自动的帮我们加上了这个基础url
+  baseURL: import.meta.env.VITE_APP_BASE_API, // 基础路径上会自动携带/api
+  timeout: 5000, // 超市的时间设置
+})
 
 // 第二步：给request实例添加请求与响应拦截器
 request.interceptors.request.use((config) => {
-    // config配置对象，headers属性请求头，经常给服务器携带公共参数
+  // config配置对象，headers属性请求头，经常给服务器携带公共参数
 
-    // 返回配置对象
-    return config;
+  // 返回配置对象
+  return config
 })
 
 // 响应拦截器
-request.interceptors.response.use((response) => {
-
+request.interceptors.response.use(
+  (response) => {
     // 成功的回调
     // 简化数据
-    return response.data;
-}, (error) => {
+    return response.data
+  },
+  (error) => {
     //  失败的回调：处理http网络错误的回调
     // 定义一个变量存储网络错误信息
-    let message = '';
+    let message = ''
     // http状态码
-    let status = error.response.status;
+    let status = error.response.status
     switch (status) {
-        case 401:
-            message = "Token过期";
-            break;
-        case 403:
-            message = "无权访问";
-            break;
-        case 404:
-            message = "请求地址错误";
-            break;
-        case 500:
-            message = "服务器出现问题";
-            break;
+      case 401:
+        message = 'Token过期'
+        break
+      case 403:
+        message = '无权访问'
+        break
+      case 404:
+        message = '请求地址错误'
+        break
+      case 500:
+        message = '服务器出现问题'
+        break
     }
     ElMessage({
-        type: 'error',
-        // kv一致，省略v
-        message
-    });
-    return Promise.reject(error);
-});
+      type: 'error',
+      // kv一致，省略v
+      message,
+    })
+    return Promise.reject(error)
+  },
+)
 
-export default request;
-
+export default request

cd0a687dd328fd4e6d2b5b7c114ee77d26e0ff89
Tom <1755619015@qq.com>
2023-05-26 16:04:06 +0800

    feat: add cartoon for mainComponent

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <!-- 路由组件的出口位置 -->
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <!-- 渲染layout一级路由组件的子路由 -->
+      <!-- 这样做可以在路由切换的时候，稍微的添加一些过度动画，看起来舒服点 -->
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style>
-
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
 </style>
